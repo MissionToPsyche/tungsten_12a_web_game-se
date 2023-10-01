f4d5aa241f589a5b35ebc24220389337f6bd5134
Bryant J. Lopez Roman <bryant.lopezroman@outlook.com>
2023-10-01 15:39:32 -0400

    clear warnings

--- Assets/2. Scripts/playerMovement.cs
+++ Assets/2. Scripts/playerMovement.cs
@@ -24,18 +24,25 @@ public class PlayerMovement : MonoBehaviour {
         horiozntal = Input.GetAxisRaw("Horizontal");
         
         if (Input.GetButtonDown("Jump") && IsGrounded()) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            if (rb.bodyType != RigidbodyType2D.Static) {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            }
         }
 
+
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            if (rb.bodyType != RigidbodyType2D.Static) {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
         }
 
         Flip();
     }
 
     private void FixedUpdate() {
-        rb.velocity = new Vector2(horiozntal * speed, rb.velocity.y);
+        if (rb.bodyType != RigidbodyType2D.Static) {
+            rb.velocity = new Vector2(horiozntal * speed, rb.velocity.y);
+        }
     }
 
     private bool IsGrounded() {
