cedadf30eeeb9c880e2880b16f1f82041bc55022
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2023-09-30 23:56:32 -0400

    First Linecast and Physics Debug Extension

--- Assets/Scripts/playerMagnet.cs
+++ Assets/Scripts/playerMagnet.cs
@@ -1,27 +1,34 @@
 using UnityEngine;
+using Physics = RotaryHeart.Lib.PhysicsExtension.Physics2D;
+using RotaryHeart.Lib.PhysicsExtension;
+using System.Net;
 
 public class playerMagnet : MonoBehaviour {
     public SpriteRenderer magnetRange;
     public BoxCollider2D magnetColl;
     public Rigidbody2D player;
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown("f"))
+        if (Input.GetKey("f"))
         {
-            magnetRange.enabled = true;
-            magnetColl.enabled = true;
+            //magnetRange.enabled = true;
+            //magnetColl.enabled = true;
+
+            //Collider2D[] hitColliders = 
+            //Physics.OverlapBox(transform.root.position + transform.up + transform.right * 3, new Vector2(6,1), 0, 1 << 3, PreviewCondition.Both, 1, Color.green, Color.red);
+            Physics.Linecast(transform.root.position + transform.up, transform.root.position + transform.up + transform.right * 6, 1 << 3, PreviewCondition.Both, 1, Color.green, Color.red);
         }
 
-        if (Input.GetKeyUp("f"))
-        {
-            magnetRange.enabled = false;
-            magnetColl.enabled = false;
-        }
+        //if (Input.GetKeyUp("f"))
+        //{
+        //    //magnetRange.enabled = false;
+        //    //magnetColl.enabled = false;
+        //}
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("HIT");
-    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("HIT");
+    //}
 }
