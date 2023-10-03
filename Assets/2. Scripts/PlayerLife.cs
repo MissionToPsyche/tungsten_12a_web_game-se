372f547805f5a3500df1460e8fd3e0f6aac45e36
unknown <mcmyers4@asu.edu>
2023-10-03 15:36:04 -0700

    Added respawn checkpoint instead of restart level on death

--- Assets/2. Scripts/PlayerLife.cs
+++ Assets/2. Scripts/PlayerLife.cs
@@ -8,12 +8,14 @@ public class PlayerLife : MonoBehaviour
 {
     private Animator animat;
     private Rigidbody2D rb2d;
+    private Vector3 respawnPoint;
 
     // Start is called before the first frame update
     private void Start()
     {
         animat = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -21,17 +23,36 @@ public class PlayerLife : MonoBehaviour
         if (collision.gameObject.CompareTag("Trap"))
         {
             Die();
+            //rb2d.bodyType = RigidbodyType2D.Static;
+            //animat.SetTrigger("death");
+            //rb2d.bodyType = RigidbodyType2D.Static;
+            //transform.position = respawnPoint;
+            //Die();
+        }
+        //transform.position = respawnPoint;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            respawnPoint = transform.position;
         }
     }
 
     private void Die()
     {
         animat.SetTrigger("death");
-        rb2d.bodyType = RigidbodyType2D.Static;
+        //rb2d.bodyType = RigidbodyType2D.Static;
+        //transform.position = respawnPoint;
     }
 
     private void RestartLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        animat.SetTrigger("respawn");
+        transform.position = respawnPoint;
+        //animat.SetTrigger("respawn");
+        //rb2d.bodyType = RigidbodyType2D.Kinematic;
     }
 }
