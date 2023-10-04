419b7738a163c17c72c449892b606df5d46e6aaa
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2023-10-03 23:37:25 -0400

    dial1

--- Assets/Scripts/dialogueController.cs
+++ Assets/Scripts/dialogueController.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class dialogueController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text Text;
+    public void SetText(string dial)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Text.SetText(dial);
     }
 }
