667fdf3efa6c39136624f9caaaf803be8804ce17
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2023-10-04 19:20:27 -0400

    Dialogue box implementation

--- Assets/Scripts/dialogueController.cs
+++ Assets/Scripts/dialogueController.cs
@@ -1,3 +1,9 @@
+/** 
+Description: Functions for the dialogue box
+Author: jmolive8
+Version: 1
+**/
+
 using TMPro;
 using UnityEngine;
 
@@ -7,5 +13,14 @@ public class dialogueController : MonoBehaviour
     public void SetText(string dial)
     {
         Text.SetText(dial);
+        gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
