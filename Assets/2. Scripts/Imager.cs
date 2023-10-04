0b7bbc69936b232bb0812b5a009f42f5e6f804b1
unknown <mcmyers4@asu.edu>
2023-10-04 00:18:07 -0700

    Added Imagers to pickup that expand FOV

---
+++ Assets/2. Scripts/Imager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imager : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.Rendering.Universal.Light2D imager;
+    [SerializeField] private float rangeIncrease = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Imager"))
+        {
+            if (imager != null)
+            {
+                imager.pointLightInnerRadius += rangeIncrease;
+                imager.pointLightOuterRadius += rangeIncrease;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
