c27ec98992e7ce1bb667d252e2c0acdaaf43fd90
JoshBenn <jdbenn@asu.edu>
2023-10-07 10:25:04 -0700

    Thruster code has been complete.  Resolved longstanding but where thruster force wouldn't be applied properly and it was due to the time step.

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -6,6 +6,12 @@ public class Player_Movement : MonoBehaviour
 {
     private Rigidbody2D character;
 
+    public Transform groundCheck;  
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private bool isGrounded;  
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -15,13 +21,20 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        // Horizontal Movement
         float horizontalDirection = Input.GetAxisRaw("Horizontal");
-
         character.velocity = new Vector2(horizontalDirection * 7f, character.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        // Jump
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             character.velocity = new Vector2(character.velocity.x, 10f);
+        }//Activate Thruster
+        else if (!isGrounded && Input.GetButton("Jump"))
+        {
+            character.velocity += new Vector2(0f, 2f * Time.deltaTime * 10f);
         }
     }
 }
