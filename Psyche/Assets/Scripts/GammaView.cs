f0053ce07d37d9db85ff73d6c5af00bb1964f460
Bryant J. Lopez Roman <bryant.lopezroman@outlook.com>
2023-10-14 02:11:38 -0400

    main logic set for tool

--- Psyche/Assets/Scripts/GammaView.cs
+++ Psyche/Assets/Scripts/GammaView.cs
@@ -4,59 +4,62 @@ Author: blopezro
 Version: 20231013
 **/
 
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GammaView : MonoBehaviour {
-    
-    // used to hold the sprites we want to change the color of
-    public SpriteRenderer[] spriteRendererArray;
 
-    // used to hold the new colors for the sprites
-    public Color[] newColorArray;
+    public GameObject[] sceneObjects; // holds all current game objects in the scene
+    public List<SpriteRenderer> spriteRenderersList; // holds all sprite renderers of game objects
+    public Color[] origColorArray; // holds original colors of the sprite renderers
 
-    // used to hold the original colors (we do not need to set these in the UI)
-    // just need to make sure we have the same size as spriteRendererArray
-    public Color[] origColorArray; 
+    // temp testing color
+    public Color tempColor = Color.green;
 
     // Start is called before the first frame update
     void Start() {
-    
-    // used to store current objects in scene 
-    GameObject[] sceneObjects = (GameObject[]) Object.FindObjectsOfType(typeof(GameObject));    
-    PrintSceneObjInfo(sceneObjects);
+        sceneObjects = (GameObject[])UnityEngine.Object.FindObjectsOfType(typeof(GameObject));
+        CaptureSpriteRenderers(sceneObjects);
 
+        // resizes based on sprite renderers in the scene
+        Array.Resize(ref origColorArray, spriteRenderersList.Count);
+        
         // captures original colors of sprites
-        for (int i = 0; i < spriteRendererArray.Length; i++) {
-            origColorArray[i] = spriteRendererArray[i].color;
+        for (int i = 0; i < spriteRenderersList.Count; i++) {
+                origColorArray[i] = spriteRenderersList[i].color;
         }
     }
 
     // Update is called when keyboard keys are pressed
     void Update() {
+        // sets and shows new color
         if (Input.GetKeyDown(KeyCode.G)) {
-            for (int i = 0; i < spriteRendererArray.Length; i++) {
-                spriteRendererArray[i].color = newColorArray[i];
+            for (int i = 0; i < spriteRenderersList.Count; i++) {
+                spriteRenderersList[i].color = tempColor;
             }
         }
 
+        // reverts to original color
         if (Input.GetKeyUp(KeyCode.G)) {
-            for (int i = 0; i < spriteRendererArray.Length; i++) {
-                spriteRendererArray[i].color = origColorArray[i];
+            for (int i = 0; i < spriteRenderersList.Count; i++) {
+                spriteRenderersList[i].color = origColorArray[i];
             }
         }
     }
 
-    // prints out information for objects in the current scene
-    void PrintSceneObjInfo(GameObject[] sceneObjects) {
+    // adds to the list the sprite renderers from the game objects in the scene
+    void CaptureSpriteRenderers(GameObject[] sceneObjects) {
         foreach (GameObject obj in sceneObjects) {
             if (obj.GetComponent(typeof(SpriteRenderer))) {
-                print("SpriteRenderer found");
-                print(obj.name);
-                print(obj.layer);
+                spriteRenderersList.Add((SpriteRenderer) obj.GetComponent(typeof(SpriteRenderer)));
             }           
         }
+    }
+
+    // returns the game objects layer
+    int GetLayer(GameObject obj) {
+        return obj.layer;
     }
 
 }
