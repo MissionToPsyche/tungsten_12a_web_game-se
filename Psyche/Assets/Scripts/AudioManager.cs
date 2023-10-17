acf56383d0efba8f251c663d573803c00a97f1a5
Bryant J. Lopez Roman <bryant.lopezroman@outlook.com>
2023-10-17 18:32:57 -0400

    add draft ver of AudioManager

---
+++ Psyche/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+/** 
+Description: audio manager script
+Author: blopezro
+Version: 20231016
+**/
+
+using UnityEngine.Audio;
+using UnityEngine;
+
+/// <summary>
+/// AudioManager class which handles all audio within game.
+/// </summary>
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField] private AudioSource audioSource0;
+    [SerializeField] private AudioSource audioSource1;
+    [SerializeField] private AudioSource audioSource2;
+    [SerializeField] private AudioSource audioSource3;
+    [SerializeField] private AudioSource audioSource4;
+    [SerializeField] private AudioSource audioSource5;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+    }
+}
