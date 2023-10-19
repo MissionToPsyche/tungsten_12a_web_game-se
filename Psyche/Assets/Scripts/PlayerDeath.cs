592e5306b5eed92420e912ea08c7053b38cafca3
unknown <mcmyers4@asu.edu>
2023-10-18 17:55:43 -0700

    Create checkpoints and add respawn logic

--- Psyche/Assets/Scripts/PlayerDeath.cs
+++ Psyche/Assets/Scripts/PlayerDeath.cs
@@ -14,6 +14,19 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class PlayerDeath : MonoBehaviour
 {
+    //Respawn location
+    private Vector3 respawnPoint;
+
+    //Initial character location when level first begins
+    private Vector3 startPoint;
+
+    //Initialize respawn point and set starting location
+    private void Start()
+    {
+        startPoint = transform.position;
+        respawnPoint = transform.position;
+    }
+
     //Player touches hazard object
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,10 +36,23 @@ public class PlayerDeath : MonoBehaviour
         }
     }
 
-    //Restart level when player dies
+    //Updates respawn location once a checkpoint has been touched
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            respawnPoint = transform.position;
+        }
+    }
+
+    //Respawn character to last checkpoint when player dies
     private void Die()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (startPoint.Equals(respawnPoint))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        transform.position = respawnPoint;
     }
 
 }
