5f9195c658330c215cd31f6a4fff7bd52466e939
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2023-10-19 00:39:28 -0400

    Inventory v1
    Reworked inventory to work with new code. Consolidated Inventory and DialogueBox scripts into UIController. Made pickup objects to activate tools. Made Prefabs for UI, Pickupables, and Player.

--- Psyche/Assets/Scripts/PickupableTool.cs
+++ Psyche/Assets/Scripts/PickupableTool.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 /// </summary>
 /// Author: jmolive8
 public class PickupableTool : MonoBehaviour {
-    public PlayerManagement pManage;
     public string toolName;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        pManage.toolPickedUp(toolName);
+        other.GetComponent<PlayerManagement>().toolPickedUp(toolName);
         Destroy(gameObject);
     }
 }
