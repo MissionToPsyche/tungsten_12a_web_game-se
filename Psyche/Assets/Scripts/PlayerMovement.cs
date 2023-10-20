0459655ed30d784c0361a1c01eeddfc021e7478a
dnguye99asu <dnguye99@asu.edu>
2023-10-20 01:07:39 -0700

    updated PlayerMovement.cs to include animation triggers for walk and jump

--- Psyche/Assets/Scripts/PlayerMovement.cs
+++ Psyche/Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,28 @@ using UnityEngine;
 ///</summary>
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator animator;
+
     public void handleMovement(Rigidbody2D playerCharacter, bool isGrounded)
-    {        
+    {
+        animator = GetComponent<Animator>();
+
         //Horizontal Movement
         float horizontalDirection = Input.GetAxisRaw("Horizontal");
         playerCharacter.velocity = new Vector2(horizontalDirection * 7f, playerCharacter.velocity.y);
+        animator.Play("player_walk");
 
         //Vertical "jump" only if player is on the ground
         if (isGrounded && Input.GetButtonDown("Jump"))
+        {
             playerCharacter.velocity = new Vector2(playerCharacter.velocity.x, 7f);
+            animator.Play("player_jump");
             //Plays jump sound
             /**AudioManager audioManager = GameObject
                 .FindGameObjectWithTag("AudioSources")
                 .GetComponent<AudioManager>();
             audioManager.PlayPlayerJump();**/
+        }
+
     }
 }
