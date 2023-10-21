f9c7006da76308cc1c761997875928d231efb921
Bryant J. Lopez Roman <bryant.lopezroman@outlook.com>
2023-10-20 22:06:17 -0400

    working batt drain

--- Psyche/Assets/Scripts/Battery.cs
+++ Psyche/Assets/Scripts/Battery.cs
@@ -6,12 +6,17 @@ Version: 20231020
 
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Battery class in which a new batt can be created, drained, and charged. 
+/// </summary>
 public class Battery : MonoBehaviour {
 
     public float batteryPercentage;
     public float rate;
+    public TMP_Text battPerText;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
@@ -27,14 +32,21 @@ public class Battery : MonoBehaviour {
     public void DrainBatt(float rate) {
         batteryPercentage -= Time.deltaTime * rate;
         batteryPercentage = Mathf.Clamp(batteryPercentage, 0f, 100f); // keeps batt level between 0-100
-        Debug.Log("Battery % "+ batteryPercentage);
+        UpdateSceneText();
+        //Debug.Log("Battery % "+ batteryPercentage);
     }
 
     // charges battery at given rate
     public void ChargeBatt(float rate) {
         batteryPercentage += Time.deltaTime * rate;
         batteryPercentage = Mathf.Clamp(batteryPercentage, 0f, 100f); // keeps batt level between 0-100
-        Debug.Log("Battery % "+ batteryPercentage);
+        UpdateSceneText();
+        //Debug.Log("Battery % "+ batteryPercentage);
+    }
+
+    // update text for scene
+    public void UpdateSceneText() {
+        battPerText.text = Mathf.RoundToInt(batteryPercentage).ToString() + "%";
     }
 
 }
