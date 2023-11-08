8f41691f984a2b0fe1108d2302b8d2a4c1d870ed
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2023-11-07 23:16:01 -0500

    Intro Cutscene v1
    Made basic cutscene for introduction. Title screen now launches to intro cutscene.

---
+++ Psyche/Assets/Scripts/Title-Cutscene/IntroController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    public GameObject intro1, intro2, intro3, intro4;
+
+    int curScreen = 1;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            switch (curScreen)
+            {
+                case 1:
+                    curScreen++;
+                    intro1.SetActive(false);
+                    intro2.SetActive(true);
+                    break;
+
+                case 2:
+                    curScreen++;
+                    intro2.SetActive(false);
+                    intro3.SetActive(true);
+                    break;
+
+                case 3:
+                    curScreen++;
+                    intro3.SetActive(false);
+                    intro4.SetActive(true);
+                    break;
+
+                default:
+                    SceneManager.LoadScene("SampleScene");
+                    break;
+            }
+        }
+    }
+}
