5adca30583bc2be1870066900e7f3fefd4679a13
JoshBenn <jdbenn@asu.edu>
2023-11-08 23:48:03 -0700

    Created abstract controller script

---
+++ Psyche/Assets/Scripts/Controller.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controller abstract script
+/// </summary>
+public abstract class Controller : MonoBehaviour
+{
+
+    //TODO
+
+}
