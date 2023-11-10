8f5ed6bfcdaf202c236adb22249754065cab302a
JoshBenn <jdbenn@asu.edu>
2023-11-10 06:34:34 -0700

    GameController and GameMaster created.  GameMaster will initialize all of the controllers.  Awake and donotdestroy centralized to BaseController class.  Controller renamed to BaseController.

---
+++ Psyche/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : BaseController<GameController>
+{
+    //Private variables
+    
+
+    //Public variables
+    //public DeveloperConsole developerConsole;
+    //public StateManager stateManager;
+    //public SceneManager sceneManager;
+    //public AudioManager audioManager;
+
+
+    protected override void InitializeController()
+    {
+        base.InitializeController();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Implement the logic for update here
+        
+    }
+}
