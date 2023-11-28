56c9e96476d99fbaed02491570843e08e3719595
JoshBenn <jdbenn@asu.edu>
2023-11-28 13:13:59 -0700

    Completed the element set command function and implemented the tool set function.  Now can set elements with set element_name.  Now can enable/disable tools with set toolname value.

--- Psyche/Assets/Scripts/Player/InventoryManager.cs
+++ Psyche/Assets/Scripts/Player/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -36,6 +37,28 @@ public class InventoryManager : MonoBehaviour
             { "element_gold", 0 },
             { "element_platinum", 0 },
         };
+    }
+
+    public void ReceiveMessage(ArrayList args)
+    {
+        int i = 0;
+        foreach(var arg in args)
+        {
+            Debug.Log(i++ + " " + arg.ToString());
+        }
+        string item = args[2].ToString().ToLower();
+        if(_tools.ContainsKey(item))
+        {
+            SetTool(item, bool.Parse(args[3].ToString()));
+        } 
+        else if (_elements.ContainsKey(item))
+        {
+            SetElement(item, int.Parse(args[3].ToString())) ;
+        }
+        else
+        {
+            Debug.Log("Incorrect item name passed -- InventoryManager");
+        }
     }
 
     /// <summary>
