c3fff0aab8fc158f81bd56c8b876610007bc01ed
JoshBenn <jdbenn@asu.edu>
2023-12-02 14:35:00 -0700

    Created the necessary dictionaries involved and determined that using the object type is the best way to handle generic values in the objectStates

--- Psyche/Assets/Scripts/GameStateTest.cs
+++ Psyche/Assets/Scripts/GameStateTest.cs
@@ -2,15 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Test integration for a gamestate class to manage the state of a scene
+/// </summary>
 public class GameStateTest
 {
+    //Use C#'s <object> type to store generic values (bool, int, etc.)
+    private Dictionary<string, object> objectStates;
+
+    /// <summary>
+    /// Constructor for the gamestate
+    /// </summary>
     public GameStateTest()
+    {
+        objectStates = new Dictionary<string, object>();
+    }
+
+    /// <summary>
+    /// Holds the default game state for the scene, allowing for a state reset
+    /// </summary>
+    public void LoadDefaultGameState()
     {
 
     }
 
-    public void LoadDefaultGameState()
+    /// <summary>
+    /// Returns the state of the scene
+    /// - Must cast the objects into their respective types (e.g. (int))
+    /// </summary>
+    /// <returns></returns>
+    public Dictionary<string, object> GetGameState()
     {
-
+        return objectStates;
     }
 }
