056e02bff8addbcc8c337ee70f31f76bb90cf8de
Kokurio <sublimeibanezkokurio@gmail.com>
2024-02-03 16:35:18 -0700

    Reconfigured the default implementation for EMagnet scene and moved some functionality to the BaseState class to ensure it properly manages default, saved, and current states.

--- Psyche/Assets/Scripts/GameStates/BaseState.cs
+++ Psyche/Assets/Scripts/GameStates/BaseState.cs
@@ -1,17 +1,71 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class BaseState
 {
     //Use C#'s <object> type to store generic values (bool, int, etc.)
-    protected Dictionary<string, object> _objectStates;
+    protected Dictionary<string, object> _defaultState;
+    protected Dictionary<string, object> _sceneState;
+    protected Dictionary<string, object> _savedState;
 
     /// <summary>
     /// Create the base class
     /// </summary>
-    public BaseState()
+    public BaseState() { }
+
+    /// <summary>
+    /// Holds the default game state for the scene, allowing for a state reset
+    /// </summary>
+    public void LoadDefaultState() 
     {
-        _objectStates = new Dictionary<string, object>();
+        _sceneState = new Dictionary<string, object>();
+
+        foreach (var pair in _defaultState)
+        {
+            if (pair.Value is ICloneable clone)
+            {
+                _sceneState[pair.Key] = clone.Clone();
+            }
+            else
+            {
+                _sceneState[pair.Key] = pair.Value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Saves the current state of the scene
+    /// </summary>
+    public void SaveState() 
+    {
+        _savedState = new Dictionary<string, object>();
+
+        foreach (var pair in _sceneState)
+        {
+            if (pair.Value is ICloneable clone)
+            {
+                _savedState[pair.Key] = clone.Clone();
+            }
+            else
+            {
+                _savedState[pair.Key] = pair.Value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Loads the saved state into the scene
+    /// </summary>
+    public virtual void LoadState() { }
+
+    /// <summary>
+    /// Sets the state of an object in the scene
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void SetObjectState(string key, object value) 
+    {
+        _sceneState[key] = value;
     }
 }
