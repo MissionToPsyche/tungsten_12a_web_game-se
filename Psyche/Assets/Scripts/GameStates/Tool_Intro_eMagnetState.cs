9a73a8d2811ca50c0fd5030ea34da8c40f694bdb
Kokurio <sublimeibanezkokurio@gmail.com>
2024-02-03 17:43:00 -0700

    Reconfigured some of the dictionary implementations -- the key is now a short allowing for the use and creation of an enum for each scene to track its objects and use strings as minimally as possible.  Match() now created in teh base class and intended to be overridden by the derived classes; match converts the provided object name into the respective short for simplicity.

--- Psyche/Assets/Scripts/GameStates/Tool_Intro_eMagnetState.cs
+++ Psyche/Assets/Scripts/GameStates/Tool_Intro_eMagnetState.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 
 /// <summary>
@@ -8,26 +10,52 @@ using UnityEngine;
 public class Tool_Intro_eMagnetState : BaseState
 {
     /// <summary>
+    /// Defines the tracked objects in the scene
+    /// -- Purpose is to reduce the potential for errors in object identification
+    /// -- Identified as `stort` (i16) for ease of use -- will use `-1` for errorneous values
+    /// </summary>
+    public enum SceneObject : short
+    {
+        ELECTRO_MAGNET_PICKUP_VARIANT = 0,
+        CHECKPOINT_0 = 1,
+        CHECKPOINT_1 = 2,
+        IRON_MOVEABLE = 3,
+        BASIC_MOVING_HAZARD = 4,
+    }
+
+    /// <summary>
+    /// Matches the input object string with its enum variant -- useful for consistency
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    protected override short Match(string obj)
+    {
+        switch (obj)
+        {
+            case "Electromagnet Pickup Variant": return (short)SceneObject.ELECTRO_MAGNET_PICKUP_VARIANT;
+            case "Checkpoint 0": return (short)SceneObject.CHECKPOINT_0;
+            case "Checkpoint 1": return (short)SceneObject.CHECKPOINT_1;
+            case "Iron Moveable": return (short)SceneObject.IRON_MOVEABLE;
+            case "Basic Moving Hazard": return (short)SceneObject.BASIC_MOVING_HAZARD;
+        }
+        return -1;
+    }
+
+    /// <summary>
     /// Constructor for the scene state
     /// </summary>
     public Tool_Intro_eMagnetState()
     {
         // This must be set up first before anything else is created as everything else is based off of this
-        _defaultState = new Dictionary<string, object>
+        _defaultState = new Dictionary<short, object>
         {
-                { "Electromagnet Pickup Variant", true }, // 'true' for available?
-                { "Checkpoint 0", false },  // 'false' for not activated
-                { "Checkpoint 1", false },
-                { "Iron Moveable", new Vector3(56.69f, -3.1f, -1.0f) },
-                { "Basic Moving Hazard", new Vector3(29.24f, 3.48f, 0f) }
+                { (short)SceneObject.ELECTRO_MAGNET_PICKUP_VARIANT, true }, // 'true' for available
+                { (short)SceneObject.CHECKPOINT_0, true },  // `true` for activate-able
+                { (short)SceneObject.CHECKPOINT_1, true },
+                { (short)SceneObject.IRON_MOVEABLE, new Vector3(56.69f, -3.1f, -1.0f) },
+                { (short)SceneObject.BASIC_MOVING_HAZARD, new Vector3(29.24f, 3.48f, 0f) }
         };
         LoadDefaultState();
         SaveState();
-    }
-
-
-    public override void LoadState()
-    {
-        // Do stuff to load the state here
     }
 }
