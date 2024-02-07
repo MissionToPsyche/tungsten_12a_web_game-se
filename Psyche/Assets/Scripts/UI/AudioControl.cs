11b7f6b4d10880f366efd77d0621baeeed1d9ce1
LopezRoman <Bryant.LopezRoman@outlook.com>
2024-02-06 23:01:09 -0500

    add audio sliders

--- Psyche/Assets/Scripts/UI/AudioControl.cs
+++ Psyche/Assets/Scripts/UI/AudioControl.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Audio control for game
 /// </summary>
 /// Author: blopezro
-public class AudioControl : MonoBehaviour
-{
+public class AudioControl : MonoBehaviour {
     public GameObject checkMark;
+    public Slider slider;
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         checkMark.SetActive(GameController.Instance.colorBlindMode);
     }
 
-    /// <summary>
-    /// Toggles check mark
-    /// </summary>
-    public void toggleCheckMark()
-    {
-        checkMark.SetActive(!checkMark.activeInHierarchy);
-        GameController.Instance.ToggleBackgroundAudioMute();
+    public void setUiVolume() {
+        GameController.Instance.audioManager.SetAudioVolumeForAllUi(slider.value);
+    }
+
+    public void setMusicVolume() {
+        GameController.Instance.audioManager.SetAudioVolumeForAllMusic(slider.value);
+    }
+
+    public void setSfxVolume() {
+        GameController.Instance.audioManager.SetAudioVolumeForAllSfx(slider.value);
     }
 }
