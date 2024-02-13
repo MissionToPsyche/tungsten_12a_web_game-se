8ab78eaf88e37dc7f71cc47bdbb151a04ce953ca
Kokurio <sublimeibanezkokurio@gmail.com>
2024-02-12 20:10:09 -0700

    PlayerController logic has been moved to the new trigger tracking script and ToolPickup has been moved to inventoryManager.  Next steps are to rework the configurations of the tools in the scenes and UI controller transition to using enum values and not strings for tracking

--- Psyche/Assets/Scripts/Player/PlayerCollisionManager.cs
+++ Psyche/Assets/Scripts/Player/PlayerCollisionManager.cs
@@ -10,9 +10,72 @@ public class PlayerCollisionManager : MonoBehaviour
     private PlayerController _playerController;
 
     // Events
-    public event Action<Collision2D> OnCollisionEnter;
-    public event Action<Collision2D> OnCollisionExit;
-    public event Action<Collision2D> OnCollisionStay;
+    public event Action<string> InitiateTransition;
+    //public event Action<Collision2D> OnCollisionEnter;
+    //public event Action<Collision2D> OnCollisionExit;
+    //public event Action<Collision2D> OnCollisionStay;
+
+    public enum CollisionName
+    {
+        Health,
+
+        None,
+    }
+
+    public CollisionName MatchName(string name)
+    {
+        return name.ToLower() switch
+        {
+            "health" or "health pickup variant" => CollisionName.Health,
+            _ => CollisionName.None,
+        };
+    }
+
+    public string MatchName(CollisionName name)
+    {
+        return name switch
+        {
+            CollisionName.Health => "Health",
+            _ => null,
+        };
+    }
+
+    public enum CollisionTag
+    {
+        // Pickupable
+        Element,
+        Tool,
+
+        // Transition Object Directions
+        TransitionObjectIn,
+        TransitionObjectOut,
+
+        None,
+    }
+
+    public CollisionTag MatchTag(string tag)
+    {
+        return tag.ToLower() switch
+        {
+            "element" => CollisionTag.Element,
+            "tool" => CollisionTag.Tool,
+            "transitionobjectin" => CollisionTag.TransitionObjectIn,
+            "transitionobjectout" => CollisionTag.TransitionObjectOut,
+            _ => CollisionTag.None,
+        };
+    }
+
+    public string MatchTag(CollisionTag tag)
+    {
+        return tag switch
+        {
+            CollisionTag.Element => "Element",
+            CollisionTag.Tool => "Tool",
+            CollisionTag.TransitionObjectIn => "TransitionObjectIn",
+            CollisionTag.TransitionObjectOut => "TransitionObjectOut",
+            _ => null,
+        };
+    }
 
 
     public void Initialize(PlayerController playerController)
@@ -20,34 +83,74 @@ public class PlayerCollisionManager : MonoBehaviour
         _playerController = playerController;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         // Enter stuff Stuff
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D other)
     {
         // Stuff
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D other)
     {
         // Enter the rest of the stuff
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Trigger enter stuff
+        Debug.Log($"{other.name}, {other.tag}");
+        switch (MatchName(other.name))
+        {
+            case CollisionName.Health:
+                _playerController.playerHealth.HealthUp(1);
+                //return; // Return out early for the time being
+                break;
+
+            default:
+                break;
+        }
+
+        switch (MatchTag(other.tag))
+        {
+            case CollisionTag.Element:
+                _playerController.inventoryManager.AddElement(other.name, 1);
+                Destroy(other.gameObject); //remove the element from the screen
+                break;
+
+            case CollisionTag.Tool:
+                _playerController.inventoryManager.ToolPickUp(other.name);
+                Destroy(other.gameObject);
+                break;
+
+            default:
+                break;
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D other)
     {
         // Trigger Exit Stuff
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        // Trigger stay stuff
+        switch (other.name)
+        {
+            default:
+                break;
+        }
+
+        switch (MatchTag(other.tag))
+        {
+            case CollisionTag.TransitionObjectIn or CollisionTag.TransitionObjectOut:
+                InitiateTransition?.Invoke(other.name);
+                break;
+
+            default:
+                break;
+        }
     }
 
 
