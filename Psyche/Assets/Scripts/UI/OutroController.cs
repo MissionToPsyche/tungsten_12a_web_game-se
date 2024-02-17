784f88f7a8bd911307868ca63acc42b1ba9d94e1
LopezRoman <Bryant.LopezRoman@outlook.com>
2024-02-17 13:07:13 -0500

    update scripts for outro

--- Psyche/Assets/Scripts/UI/OutroController.cs
+++ Psyche/Assets/Scripts/UI/OutroController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Basic Outro Cutscene that shows an image
 /// </summary>
-/// Author: jmolive8
-public class OutroController : MonoBehaviour
-{
+/// Author: jmolive8, blopezro
+public class OutroController : MonoBehaviour {
+
+    /// <summary>
+    /// Gets rid of objects not needed anymore
+    /// </summary>
+    void Awake() {
+        //UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        Destroy(GameObject.FindGameObjectWithTag("UI"));
+    }
+
     /// <summary>
     /// Returns to Title Screen when the button is pressed
     /// </summary>
     public void QuitGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Title_Screen");
+        GameController.Instance.audioManager.buttonClick.Play();
     }
+
 }
