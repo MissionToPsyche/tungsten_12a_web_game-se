521854d29d5574c9db6e1f28159bdadbeec2d872
LopezRoman <Bryant.LopezRoman@outlook.com>
2024-02-24 23:04:58 -0500

    added note

--- Psyche/Assets/Scripts/SceneTransitionManager.cs
+++ Psyche/Assets/Scripts/SceneTransitionManager.cs
@@ -170,7 +170,11 @@ public class SceneTransitionManager : MonoBehaviour {
     public Vector3 GetTransitionObjectPosition(string sceneName) {
         Scene scene = SceneManager.GetSceneByName(sceneName);
         if (scene.isLoaded) {
-            string tag = _directionTag.ToLower().Contains("in") ? "TransitionObjectOut" : "TransitionObjectIn";
+
+            // If the tag is "out" as in if "out" is part of the game object the player is interacting with
+            // then we want to "look for" the TransitionObjectIn (in) on the scene we are going into, else
+            // we must be going in reverse so then we look for the TransitionObjectOut (out).
+            string tag = _directionTag.ToLower().Contains("out") ? "TransitionObjectIn" : "TransitionObjectOut";
 
             // there should only be one transition object in and one transition object out for each scene
             // with the exception of two "outs" for the landing scene since we can go out from the landing scene
