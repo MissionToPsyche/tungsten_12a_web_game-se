78defb9b46d137ed884c02551f64eca1641cad1a
LopezRoman <bryant.lopezroman@outlook.com>
2024-03-26 23:20:19 -0400

    code review and format scripts

--- Psyche/Assets/Scripts/UI/AudioControl.cs
+++ Psyche/Assets/Scripts/UI/AudioControl.cs
@@ -1,29 +1,45 @@
+/**
+Description: Audio control script
+Author: blopezro
+Version: 20240326
+**/
+
 using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// Audio control for game
+/// AudioControl Class
 /// </summary>
-/// Author: blopezro
 public class AudioControl : MonoBehaviour {
     public GameObject checkMark;
     public Slider slider;
 
+    /// <summary>
+    /// Activates check mark
+    /// </summary>
     private void OnEnable() {
         checkMark.SetActive(GameController.Instance.colorBlindMode);
     }
 
+    /// <summary>
+    /// Sets the volume for all ui
+    /// </summary>
     public void setUiVolume() {
         GameController.Instance.audioManager.SetAudioVolumeForAllUi(slider.value);
-        // todo: add example play of sound
     }
 
+    /// <summary>
+    /// Sets the volume for all music
+    /// </summary>
     public void setMusicVolume() {
         GameController.Instance.audioManager.SetAudioVolumeForAllMusic(slider.value);
     }
 
+    /// <summary>
+    /// Sets the volume for all sfx
+    /// </summary>
     public void setSfxVolume() {
         GameController.Instance.audioManager.SetAudioVolumeForAllSfx(slider.value);
-        // todo: add example play of sound
     }
+
 }
