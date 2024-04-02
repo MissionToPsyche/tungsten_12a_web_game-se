650b1e0ebf3b717db1b02775b1301137663e0d8a
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2024-04-02 19:16:08 -0400

    Title/Combo3 transitions fix

--- Psyche/Assets/Scripts/Controllers/UIController.cs
+++ Psyche/Assets/Scripts/Controllers/UIController.cs
@@ -498,12 +498,12 @@ public class UIController : BaseController<UIController>
             /**
              * Destroys Player and UI so they do not spawn on the start screen
              */
-            Destroy(PlayerController.Instance.gameObject);
-            Destroy(gameObject);
             GameController.Instance.sceneTransitionManager.devControl = true;
             GameController.Instance.sceneTransitionManager.OnInitiateTransition(
                 GameController.Instance.gameStateManager.MatchScene(GameStateManager.Scene.Title)
                 );
+            Destroy(PlayerController.Instance.gameObject);
+            Destroy(gameObject);
         }
     }
 
