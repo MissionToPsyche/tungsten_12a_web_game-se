2ea6b1dffe03da50b69871a7ac229311256a7318
jmolive8 <77370384+jmolive8@users.noreply.github.com>
2024-04-04 21:10:16 -0400

    transition logic reorder

--- Psyche/Assets/Scripts/SceneTransitionManager.cs
+++ Psyche/Assets/Scripts/SceneTransitionManager.cs
@@ -188,13 +188,18 @@ public class SceneTransitionManager : MonoBehaviour {
             // We differentiate the proper transition objects for the player with the layer as well
             // Default caves use the default layer while special case transition objects use other layers
             foreach (GameObject obj in objectsWithTransitionTag) {
-                // If the object in the list is the default transition object and we ARE NOT coming from Combo 3
-                if (obj.layer == 0 && GameController.Instance.GameStateManager.MatchScene(_travelFromSceneName) != GameStateManager.Scene.Combo3) { // default layer
-                    caveObject = obj;                   
-                    break; // break since we only need one GameObject
                 // If the object in the list is the cave layered transition object and we ARE coming from Combo 3
-                } else if (obj.layer == 8 && GameController.Instance.GameStateManager.MatchScene(_travelFromSceneName) == GameStateManager.Scene.Combo3) { // cave layer
+                if (GameController.Instance.GameStateManager.MatchScene(_travelFromSceneName) == GameStateManager.Scene.Combo3 
+                    && GameController.Instance.GameStateManager.MatchScene(sceneName) == GameStateManager.Scene.Landing
+                    && obj.layer == 8) // cave layer
+                {
                     caveObject = obj;
+                    break; // break since we only need one GameObject
+                }
+                // If the object in the list is the default transition object and we ARE NOT coming from Combo 3
+                else if (obj.layer == 0) // default layer
+                {
+                    caveObject = obj;                   
                     break; // break since we only need one GameObject
                 }
             }
